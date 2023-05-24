2bdce71c597887314f4d6893895ffe1c2d689f19
Jiří Vrba <jiri@vrba.dev>
2023-05-24 10:01:58 +0200

    Configure discord branding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        discord: {
+          black: "#1e1f22",
+          dark: "#2b2d31",
+          light: "#313338",
+          text: "#80848e",
+          primary: "#5865f2"
+        }
+      }
     },
   },
   plugins: [],
